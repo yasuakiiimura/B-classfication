cd33df331641f0c823375651dea6a64c328c546a
yasuakiiimura <46255251+yasuakiiimura@users.noreply.github.com>
2021-08-16 15:00:36 +0900

    Update train.py

--- test/train.py
+++ test/train.py
@@ -8,24 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datagen import gen_rand_data, get_label
 from network import makemodel
-from plot import  plotscatter
-
-def result_draw(test_x,test_t,rs):
-    plt.cla()
-    r_00_x = [x for (x,r,t) in zip(test_x[:,0],rs[:,0],test_t) if (r == t)and(r == 0)]
-    r_00_y = [y for (y,r,t) in zip(test_x[:,1],rs[:,0],test_t) if (r == t)and(r == 0)]
-    plt.scatter(r_00_x,r_00_y,c='red',marker='o')
-    r_01_x = [x for (x,r,t) in zip(test_x[:,0],rs[:,0],test_t) if (r != t)and(r == 0)]
-    r_01_y = [y for (y,r,t) in zip(test_x[:,1],rs[:,0],test_t) if (r != t)and(r == 0)]
-    plt.scatter(r_01_x,r_01_y,c='red',marker='x')
-    r_11_x = [x for (x,r,t) in zip(test_x[:,0],rs[:,0],test_t) if (r == t)and(r == 1)]
-    r_11_y = [y for (y,r,t) in zip(test_x[:,1],rs[:,0],test_t) if (r == t)and(r == 1)]
-    plt.scatter(r_11_x,r_11_y,c='blue',marker='o')
-    r_10_x = [x for (x,r,t) in zip(test_x[:,0],rs[:,0],test_t) if (r != t)and(r == 1)]
-    r_10_y = [y for (y,r,t) in zip(test_x[:,1],rs[:,0],test_t) if (r != t)and(r == 1)]
-    plt.scatter(r_10_x,r_10_y,c='blue',marker='x')
-    plt.savefig("result.png", format="png", dpi=300)
-    return (len(r_00_x)+len(r_11_x))/len(test_x)
+from plot import  plotscatter,result_draw
 
 Model = makemodel()
 train_x = gen_rand_data(100000)
